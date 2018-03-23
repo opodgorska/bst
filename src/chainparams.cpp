c118d048180c713a04fd09a4413e4d5efe7006f0
kuchtak <btx@hprox.com>
2018-03-23 09:27:32 +0100

    Changing consensus.nMinimumChainWork

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -105,8 +105,7 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1510704000; // November 15th, 2017.
 
         // The best chain should have at least this much work.
-        //consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000000000000000001200120012");
-	consensus.nMinimumChainWork = uint256S("0000000000000000000000000000000000000000000000000000006d006d006d");
+	consensus.nMinimumChainWork = uint256S("0000000000000000000000000000000000000000000000000000004100410041");
 
         // By default assume that the signatures in ancestors of this block are valid.
         consensus.defaultAssumeValid = uint256S("0x0000000000000000005214481d2d96f898e3d5416e43359c145944a909d242e0"); //506067
