dd4cd9ad97856b1b0bfaa30d2ec55e609403c311
Slawek Mozdzonek <s.mozdzonek@bioinfo.pl>
2018-05-09 12:08:50 +0200

    retreive string button moved up, showing data size in transaction description dialog

--- src/qt/transactiondesc.cpp
+++ src/qt/transactiondesc.cpp
@@ -242,6 +242,7 @@ QString TransactionDesc::toHTML(CWallet *wallet, CWalletTx &wtx, TransactionReco
 
     strHTML += "<b>" + tr("Transaction ID") + ":</b> " + rec->getTxID() + "<br>";
     strHTML += "<b>" + tr("Transaction total size") + ":</b> " + QString::number(wtx.tx->GetTotalSize()) + " bytes<br>";
+    strHTML += "<b>" + tr("Data size") + ":</b> " + QString::number(wtx.tx->GetOP_ReturnSize()) + " bytes<br>";
     strHTML += "<b>" + tr("Transaction virtual size") + ":</b> " + QString::number(GetVirtualTransactionSize(*wtx.tx)) + " bytes<br>";
     strHTML += "<b>" + tr("Output index") + ":</b> " + QString::number(rec->getOutputIndex()) + "<br>";
 
