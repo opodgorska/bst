af2b6a6ae142c705ef9b0a847b6ff5eb983df753
Slawek Mozdzonek <s.mozdzonek@bioinfo.pl>
2018-08-02 11:30:57 +0200

    correcting rpcnestedtests in qt

--- src/qt/test/rpcnestedtests.cpp
+++ src/qt/test/rpcnestedtests.cpp
@@ -75,7 +75,7 @@ void RPCNestedTests::rpcNestedTests()
     QVERIFY(result == result2);
 
     RPCConsole::RPCExecuteCommandLine(*node, result, "getblock(getbestblockhash())[tx][0]", &filtered);
-    QVERIFY(result == "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b");
+    QVERIFY(result == "b2f63819a53936baa63d9f42bd2a5a63dbc8475e7e63d6d0b214d332aaeedc69");
     QVERIFY(filtered == "getblock(getbestblockhash())[tx][0]");
 
     RPCConsole::RPCParseCommandLine(nullptr, result, "importprivkey", false, &filtered);
