bb244e21009978c3471ef44f1651ae1960f1eb62
Slawek Mozdzonek <s.mozdzonek@bioinfo.pl>
2018-11-06 09:55:58 +0100

    correcting functional test: p2p_invalid_tx

--- test/functional/p2p_invalid_tx.py
+++ test/functional/p2p_invalid_tx.py
@@ -64,7 +64,7 @@ class InvalidTxRequestTest(BitcoinTestFramework):
         node.p2p.send_blocks_and_test([block], node, success=True)
 
         self.log.info("Mature the block.")
-        self.nodes[0].generate(100)
+        self.nodes[0].generate(1000)
 
         # b'\x64' is OP_NOTIF
         # Transaction will be rejected with code 16 (REJECT_INVALID)
