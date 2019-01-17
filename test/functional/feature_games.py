b2e90adc46fe2d40996172799d492033ba3db32a
Tomasz Slabon <t.slabon@bioinfo.pl>
2019-01-17 16:37:15 +0100

    Added python tests for checking exceeded potential bets reward limit and block subsidy limit

--- test/functional/feature_games.py
+++ test/functional/feature_games.py
@@ -27,6 +27,12 @@ class MakeBetTest(BitcoinTestFramework):
         self.nodeA.generate(nblocks=1)
         self.sync_all()
 
+    def assert_number_of_txs_current_block(self, num_txs):
+        count = self.nodeA.getblockcount()
+        hash = self.nodeA.getblockhash(count)
+        block = self.nodeA.getblock(hash)
+        assert len(block["tx"]) == num_txs
+
     def roulette_must_succeed(self):
         bal_1 = self.nodeA.getbalance()
         self.nodeA.makebet(type_of_bet="red@1+black@1")
@@ -36,7 +42,7 @@ class MakeBetTest(BitcoinTestFramework):
         self.generate_block()
         bal_3 = self.nodeA.getbalance()
         block_amount = 50
-        assert_equal(bal_2 + block_amount, bal_3) # should it be exactly equal???
+        assert_equal(bal_2 + block_amount, bal_3)
 
         self.generate_block()
         bal_4 = self.nodeA.getbalance()
@@ -134,6 +140,50 @@ class MakeBetTest(BitcoinTestFramework):
         bal_7 = self.nodeA.getbalance()
         assert_equal(bal_6 + block_amount, bal_7)
 
+    def bet_sum_exceeds_block_subsidy(self):
+        # 90% of block subsidy is 22.5(25*0.9) at this moment
+        self.nodeA.makebet(type_of_bet="red@2+black@2")                  # 4
+        self.nodeA.makebet(type_of_bet="dozen_1@2+dozen_2@1+dozen_3@1")  # 4
+        self.nodeA.makebet(type_of_bet="even@1+odd@3")                   # 4
+        self.nodeA.makebet(type_of_bet="even@3+odd@1")                   # 4
+        self.nodeA.makebet(type_of_bet="1@1+2@1+3@1+4@1", range=4)       # 4
+        self.nodeA.makebet(type_of_bet="red@2+black@2")                  # 4
+        self.nodeA.makebet(type_of_bet="even@2+odd@2")                   # 4
+
+        self.generate_block()
+        # coinbase + 5 makebets
+        self.assert_number_of_txs_current_block(1+5)
+
+        self.generate_block()
+        # coinbase + 2 makebets + getbet
+        self.assert_number_of_txs_current_block(1+2+1)
+
+        self.generate_block()
+        # coinbase + getbet
+        self.assert_number_of_txs_current_block(1+1)
+
+        self.generate_block()
+        # coinbase
+        self.assert_number_of_txs_current_block(1)
+
+    def potential_bets_reward_exceeds_limit(self):
+        # potential bets reward limit is: 104,857,600,000,000(1024*1024*100,000,000)
+        self.nodeA.makebet(type_of_bet="1@1", range=524288)    # 52,428,800,000,000
+        self.nodeA.makebet(type_of_bet="1@1", range=524287)    # 52,428,700,000,000
+        self.nodeA.makebet(type_of_bet="red@0.25+black@0.25")  # 100,000,000
+        self.nodeA.makebet(type_of_bet="red@0.25+black@0.25")  # 100,000,000
+
+        self.generate_block()
+        # coinbase + 2 makebets
+        self.assert_number_of_txs_current_block(1+3)
+
+        self.generate_block()
+        # coinbase + 1 makebet + getbet
+        self.assert_number_of_txs_current_block(1+1+1)
+
+        self.generate_block()
+        self.generate_block()
+
     def run_test(self):
         self.nodeA = self.nodes[0]
         self.nodeB = self.nodes[1]
@@ -143,6 +193,8 @@ class MakeBetTest(BitcoinTestFramework):
         self.lottery_must_succeed()
         self.roulette_two_nodes()
         self.roulette_three_games_one_after_another()
+        self.bet_sum_exceeds_block_subsidy()
+        self.potential_bets_reward_exceeds_limit()
 
 
 if __name__ == '__main__':
